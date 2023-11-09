46a7b362c38f899f0b34025759441a83f72f99d6
esilaeva <emsilaeva@gmail.com>
2023-11-09 15:47:05 +0200

    * update browserstack.properties

--- src/test/java/config/browserstackconfig/BrowserstackConfig.java
+++ src/test/java/config/browserstackconfig/BrowserstackConfig.java
@@ -8,34 +8,26 @@ import org.aeonbits.owner.Config;
 public interface BrowserstackConfig extends Config {
 
     @Key("platformName")
-    @DefaultValue("android")
     String getPlatformName();
 
     @Key("device")
-    @DefaultValue("Samsung Galaxy S22 Ultra")
     String getDevice();
 
     @Key("os_version")
-    @DefaultValue("12.0")
     String getPlatformVersion();
 
     @Key("app")
-    @DefaultValue("bs://a43271109d15afee89307b02523f1e6bf6499e0d")
     String getApp();
 
     @Key("deviceHost")
-    @DefaultValue("browserstack")
     String getDeviceHost();
 
     @Key("projectName")
-    @DefaultValue("Wire Project")
     String getProject();
 
     @Key("build")
-    @DefaultValue("browserstack-build-1")
     String getBuild();
 
     @Key("testName")
-    @DefaultValue("android_wire_tests")
     String getTestName();
 }
